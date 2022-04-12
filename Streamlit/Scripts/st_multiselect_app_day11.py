28cd3dd24a1ebc173c521d036df3038668c1430a
asar1245 <sebasar1245@gmail.com>
2022-04-11 23:07:01 -0500

    Day 72 100DaysOfMLCode

---
+++ Streamlit/Scripts/st_multiselect_app_day11.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
---
+++ Streamlit/Scripts/st_selectbox_app_day10.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
